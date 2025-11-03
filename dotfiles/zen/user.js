7a30f7055ffb7124cc9cf36b760b16a0ce5646f8
Fahad S. <115593529+Fahad-S94@users.noreply.github.com>
2025-11-03 18:48:07 -0500

    Update user.js

--- dotfiles/zen/user.js
+++ dotfiles/zen/user.js
@@ -1,27 +1,17 @@
 //PLACE IN PROFILE FOLDER, NOT CHROME FOLDER
 // Enable custom CSS stylesheets
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-
-// Disable beforeunload dialog prompts
 user_pref("dom.disable_beforeunload", true);
-
-// Tab unloading after 5 minutes of inactivity
 user_pref("browser.tabs.min_inactive_duration_before_unload", 300000);
+user_pref("browser.tabs.searchclipboardfor.middleclick", false);
+user_pref("browser.tabs.insertAfterCurrent", true);
 
 // Zen compact view settings
 user_pref("zen.view.compact.hide-toolbar", true);
 user_pref("zen.view.compact.show-background-tab-toast", false);
 user_pref("zen.view.compact.enable-at-startup", true);
-
-// Remove content element separation
 user_pref("zen.theme.content-element-separation", 0);
-
-// Disable middle-click search from clipboard
-user_pref("browser.tabs.searchclipboardfor.middleclick", false);
-
-// Open new tabs after current tab
-user_pref("browser.tabs.insertAfterCurrent", true);
 
 // Zen interface customization
 user_pref("zen.view.experimental-no-window-controls", true);
-//user_pref("zen.view.use-single-toolbar", true);
+// user_pref("zen.view.use-single-toolbar", true);
