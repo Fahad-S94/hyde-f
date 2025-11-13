257bbc3049150af1496c34ca66f6f7dcd5c3d386
Fahad S. <115593529+Fahad-S94@users.noreply.github.com>
2025-11-13 10:26:21 -0500

    Update atuin.fish

--- Configs/.config/fish/conf.d/atuin.fish
+++ Configs/.config/fish/conf.d/atuin.fish
@@ -1,8 +1,20 @@
-set -gx ATUIN_NOBIND true
-atuin init fish | source
+if type -q atuin
+    set -gx ATUIN_NOBIND true
+    atuin init fish | source
 
-# Bind up arrow to smart function (moves to beginning OR triggers atuin)
-bind \e\[A _smart_up_arrow
-bind \eOA _smart_up_arrow
+    # Smart up arrow function that checks if we're in completion menu
+    function _smart_atuin_up
+        # Check if we're in a completion menu
+        if commandline --paging-mode
+            # In completion menu - navigate up in the menu
+            up-or-search
+        else
+            # Not in completion menu - use atuin
+            _atuin_search
+        end
+    end
 
-# Down arrow uses default behavior (normal history navigation)
+    # Bind up arrow to smart function
+    bind \e\[A _smart_atuin_up
+    bind \eOA _smart_atuin_up
+end
